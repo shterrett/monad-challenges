addd73168c0b3810418ea6b2e0feb941d5e8c146
Stuart Terrett <shterrett@gmail.com>
2016-08-30 19:52:13 -0400

    More Generators

--- src/Set1.hs
+++ src/Set1.hs
@@ -5,7 +5,9 @@ module Set1 where
 
 import MCPrelude
 
-buildList :: (Seed -> (a, Seed)) -> Integer -> Seed -> [a] -> [a]
+type Gen a = Seed -> (a, Seed)
+
+buildList :: Gen a -> Integer -> Seed -> [a] -> [a]
 buildList _ 0 _ l = l
 buildList randFn i s l =
     let (r, ns) = randFn s
@@ -14,10 +16,27 @@ buildList randFn i s l =
 fiveRands :: [Integer]
 fiveRands = buildList rand 5 (mkSeed 1) []
 
-randomLetter :: Seed -> (Char, Seed)
+randomLetter :: Gen Char
 randomLetter s =
     let (i, newSeed) = rand s
     in (toLetter i, newSeed)
 
 randString3 :: String
 randString3 = buildList randomLetter 3 (mkSeed 1) []
+
+randEven :: Gen Integer
+randEven = generalB rand (2*)
+
+randOdd :: Gen Integer
+randOdd = generalB rand (\i -> 2*i - 1)
+
+randTen :: Gen Integer
+randTen = generalB rand (10*)
+
+generalA :: (Integer -> Integer) -> Gen Integer
+generalA transform = let output = \(i, seed) -> (transform i, seed)
+                     in output . rand
+
+generalB :: Gen a -> (a -> a) -> Gen a
+generalB r t = let output = \(i, seed) -> (t i, seed)
+               in output . r
