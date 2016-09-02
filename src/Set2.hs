109505612afbe3827562325f161a61dd65c6427e
Stuart Terrett <shterrett@gmail.com>
2016-09-01 21:52:20 -0400

    Generalizing chains of failure

--- src/Set2.hs
+++ src/Set2.hs
@@ -6,10 +6,12 @@ module Set2 where
 import MCPrelude
 
 data Maybe a = Some a | None
+             deriving Eq
 
 instance Show a => Show (Maybe a) where
     show (Some a) = show a
     show None = "None"
+
 
 headMay :: [a] -> Maybe a
 headMay [] = None
@@ -52,14 +54,33 @@ queryGreek db key = case lookupMay db key of Some xs -> doMath xs
         divide h t = case maximumMay t of Some t1 -> divMay (fromInteger t1) (fromInteger h)
                                           None -> None
 
--- queryGreek greekDataA "alpha" == Some 2.0
--- queryGreek greekDataA "beta" == None
--- queryGreek greekDataA "gamma" == Some 3.3333333333333335
--- queryGreek greekDataA "delta" == None
--- queryGreek greekDataA "zeta" == None
+-- queryGreek2 greekDataA "alpha" == Some 2.0
+-- queryGreek2 greekDataA "beta" == None
+-- queryGreek2 greekDataA "gamma" == Some 3.3333333333333335
+-- queryGreek2 greekDataA "delta" == None
+-- queryGreek2 greekDataA "zeta" == None
 -- 
--- queryGreek greekDataB "rho" == None
--- queryGreek greekDataB "phi" == Some 0.24528301886792453
--- queryGreek greekDataB "chi" == Some 9.095238095238095
--- queryGreek greekDataB "psi" == None
--- queryGreek greekDataB "omega" == Some 24.0
+-- queryGreek2 greekDataB "rho" == None
+-- queryGreek2 greekDataB "phi" == Some 0.24528301886792453
+-- queryGreek2 greekDataB "chi" == Some 9.095238095238095
+-- queryGreek2 greekDataB "psi" == None
+-- queryGreek2 greekDataB "omega" == Some 24.0
+
+chain :: (a -> Maybe b) -> Maybe a -> Maybe b
+chain _ None = None
+chain f (Some x) = f x
+
+link :: Maybe a -> (a -> Maybe b) -> Maybe b
+link None _ = None
+link (Some x) f = f x
+
+(<&>) :: Maybe a -> (a -> Maybe b) -> Maybe b
+(<&>) = link
+
+queryGreek2 :: GreekData -> String -> Maybe Double
+queryGreek2 db key = (lookupMay db key) <&> divHeadTail
+  where divHeadTail xs = let denom = headMay xs
+                             num = tailMay xs <&> maximumMay
+                         in num <&> (\n -> denom <&>
+                                           (\d -> Some(fromInteger d))  <&>
+                                                  divMay (fromInteger n))
