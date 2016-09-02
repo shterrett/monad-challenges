e8977cd83f9141602d2a0ace11843bda3f481409
Stuart Terrett <shterrett@gmail.com>
2016-09-01 20:51:09 -0400

    Chaining failing computations

--- src/Set2.hs
+++ src/Set2.hs
@@ -15,10 +15,9 @@ headMay :: [a] -> Maybe a
 headMay [] = None
 headMay (x:xs) = Some x
 
-tailMay :: [a] -> Maybe a
+tailMay :: [a] -> Maybe [a]
 tailMay [] = None
-tailMay (x:[]) = Some x
-tailMay (x:xs) = tailMay xs
+tailMay (_:xs) = Some xs
 
 lookupMay :: Eq a => [(a, b)] -> a -> Maybe b
 lookupMay ls x = case firstMatch of Some (key, value) -> Some value
@@ -42,3 +41,25 @@ minimumMay (x:xs) = Some (foldl (\min a -> smaller a min) x xs)
   where
     smaller y z | y <= z = y
                 | y > z = z
+
+queryGreek :: GreekData -> String -> Maybe Double
+queryGreek db key = case lookupMay db key of Some xs -> doMath xs
+                                             None -> None
+  where doMath ys = let h = headMay ys
+                        t = tailMay ys
+                    in case (h, t) of (Some h1, Some t1) -> divide h1 t1
+                                      _ -> None
+        divide h t = case maximumMay t of Some t1 -> divMay (fromInteger t1) (fromInteger h)
+                                          None -> None
+
+-- queryGreek greekDataA "alpha" == Some 2.0
+-- queryGreek greekDataA "beta" == None
+-- queryGreek greekDataA "gamma" == Some 3.3333333333333335
+-- queryGreek greekDataA "delta" == None
+-- queryGreek greekDataA "zeta" == None
+-- 
+-- queryGreek greekDataB "rho" == None
+-- queryGreek greekDataB "phi" == Some 0.24528301886792453
+-- queryGreek greekDataB "chi" == Some 9.095238095238095
+-- queryGreek greekDataB "psi" == None
+-- queryGreek greekDataB "omega" == Some 24.0
