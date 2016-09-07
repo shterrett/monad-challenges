9cca7b3286780efbdc28f5a6c8afd4e2f4b2bd0f
Stuart Terrett <shterrett@gmail.com>
2016-09-07 05:58:46 -0400

    Poker Hands

--- src/Set3.hs
+++ src/Set3.hs
@@ -5,9 +5,23 @@ module Set3 where
 
 import MCPrelude
 
+data Card = Card Int String
+instance Show Card where
+    show (Card r s) = (show r) ++ s
+
 allPairs :: [a] -> [b] -> [(a, b)]
 allPairs [] _ = []
 allPairs _ [] = []
 allPairs (x:xs) ys = (pairs x ys) ++ (allPairs xs ys)
   where pairs e [] = []
         pairs e (z:zs) = (e, z):(pairs e zs)
+
+allCardsFromPairs :: [Int] -> [String] -> [Card]
+allCardsFromPairs ranks suits = (map (uncurry Card)) (allPairs ranks suits)
+
+allCards :: [Int] -> [String] -> [Card]
+allCards [] _ = []
+allCards _ [] = []
+allCards (x:xs) ys = (cards x ys) ++ (allCards xs ys)
+  where cards e [] = []
+        cards e (z:zs) = (Card  e z):(cards e zs)
