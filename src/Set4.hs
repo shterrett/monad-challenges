e022a8b79bb0c7c9757284fa66c00020e7a488c7
Stuart Terrett <shterrett@gmail.com>
2016-09-08 20:33:59 -0400

    General Signatures

--- src/Set4.hs
+++ src/Set4.hs
@@ -14,13 +14,16 @@ instance Show a => Show (Maybe a) where
 
 generalB :: Gen a -> Gen b -> (a -> b -> c) -> Gen c
 yLink ::  (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+yComb :: (a -> b -> c) -> m a -> m b -> m c
 
 generalA2 :: Gen a -> (a -> a) -> Gen a
 transMaybe :: (a -> b) -> Maybe a -> Maybe b
-
+pass :: (a -> b) -> m a -> m b
 
 genTwo :: Gen a -> (a -> Gen b) -> Gen b
 link :: Maybe a -> (a -> Maybe b) -> Maybe b
+through :: m a -> (a -> m b) -> m b
 
 mkGen :: a -> Gen a
 mkMaybe :: a -> Maybe a
+make :: a -> m a
