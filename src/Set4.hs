c9c82b9bf8ddb41a90d1b878224340818100c418
Stuart Terrett <shterrett@gmail.com>
2016-09-09 09:26:12 -0400

    Monad class

--- src/Set4.hs
+++ src/Set4.hs
@@ -3,6 +3,8 @@
 
 module Set4 where
 
+import MCPrelude
+
 type Gen a = Seed -> (a, Seed)
 
 data Maybe a = Some a | None
@@ -12,18 +14,30 @@ instance Show a => Show (Maybe a) where
     show (Some a) = show a
     show None = "None"
 
-generalB :: Gen a -> Gen b -> (a -> b -> c) -> Gen c
-yLink ::  (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
-yComb :: (a -> b -> c) -> m a -> m b -> m c
+-- generalB :: Gen a -> Gen b -> (a -> b -> c) -> Gen c
+-- yLink ::  (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+-- yComb :: (a -> b -> c) -> m a -> m b -> m c
+--
+-- generalA2 :: Gen a -> (a -> a) -> Gen a
+-- transMaybe :: (a -> b) -> Maybe a -> Maybe b
+-- pass :: (a -> b) -> m a -> m b
+--
+-- genTwo :: Gen a -> (a -> Gen b) -> Gen b
+-- link :: Maybe a -> (a -> Maybe b) -> Maybe b
+-- through :: m a -> (a -> m b) -> m b
+--
+-- mkGen :: a -> Gen a
+-- mkMaybe :: a -> Maybe a
+-- make :: a -> m a
 
-generalA2 :: Gen a -> (a -> a) -> Gen a
-transMaybe :: (a -> b) -> Maybe a -> Maybe b
-pass :: (a -> b) -> m a -> m b
+class Monad m where
+    bind :: m a -> (a -> m b) -> m b
+    return :: a -> m a
+    yComb :: (a -> b -> c) -> m a -> m b -> m c
+    yComb f m1 m2 = bind m2 (\v2 -> bind mg (\g -> return (g v2)))
+      where mg = bind m1 (\v1 -> return (f v1)) -- -> Monad (b -> c)
 
-genTwo :: Gen a -> (a -> Gen b) -> Gen b
-link :: Maybe a -> (a -> Maybe b) -> Maybe b
-through :: m a -> (a -> m b) -> m b
-
-mkGen :: a -> Gen a
-mkMaybe :: a -> Maybe a
-make :: a -> m a
+instance Monad Maybe where
+    return a = Some a
+    bind (Some x) f = f x
+    bind None _ = None
