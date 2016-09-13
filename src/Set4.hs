1291352cb0e6e3d10e0b1cd6439900c0d4961b03
Stuart Terrett <shterrett@gmail.com>
2016-09-12 22:06:08 -0400

    Rewrite generic functions

--- src/Set4.hs
+++ src/Set4.hs
@@ -31,8 +31,8 @@ instance Show a => Show (Maybe a) where
 class Monad m where
     bind :: m a -> (a -> m b) -> m b
     return :: a -> m a
-    yLink :: (a -> b -> c) -> m a -> m b -> m c
-    yLink f m1 m2 = bind m2 (\v2 -> bind mg (\g -> return (g v2)))
+    yLink :: m a -> m b -> (a -> b -> c) -> m c
+    yLink  m1 m2 f = bind m2 (\v2 -> bind mg (\g -> return (g v2)))
       where mg = bind m1 (\v1 -> return (f v1)) -- -> Monad (b -> c)
 
 instance Monad Maybe where
@@ -56,3 +56,22 @@ instance Monad Gen where
                       let (a, s2) = runGen ga s
                       in runGen (f a) s2)
 
+sequence :: Monad m => [m a] -> m [a]
+sequence (x:xs) = yLink (toList x) (sequence xs) (\b c -> b ++ c)
+  where toList m1 = bind m1 (\y -> return [y])
+
+liftM2 :: Monad m => m a -> m b -> (a -> b -> c) -> m c
+liftM2 = yLink
+
+liftM3 :: Monad m => m a -> m b -> m c ->  (a -> b -> c -> d) -> m d
+liftM3 m1 m2 m3 f = bind m3 (\v3 -> bind mg (\g -> return (g v3)))
+    where mg = liftM2 m1 m2 f -- Monad (c -> d)
+
+join :: Monad m => m (m a) -> m a
+join x = bind x id
+
+chain :: Monad m => (a -> m b) -> m a -> m b
+chain = flip bind
+
+ap :: Monad m => m (a -> b) -> m a -> m b
+ap mf ma = liftM2 mf ma (\f va -> f va)
